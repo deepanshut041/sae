bd8f2f3b74b3c903e50f1986b6fa7845e768fbd9
deepanshut041 <deepanshut041@gmail.com>
2018-01-05 02:20:44 +0530

    Made navbar now working on main page

--- client/src/app/core/navbar/navbar.component.ts
+++ client/src/app/core/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit,AfterViewInit } from "@angular/core";
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.scss"]
+  styleUrls: ["./navbar.component.css"]
 })
 
 export class NavbarComponent implements OnInit {
@@ -17,14 +17,20 @@ export class NavbarComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-    // var nav = document.getElementById('navbar');
-    // window.addEventListener("scroll", function () {
-    //   if (document.documentElement.scrollTop >= 200) {
-    //     nav.classList.add("bg-dark");
-    //     console.log("event fired")
-    //   } else {
-    //     nav.classList.remove("bg-dark");
-    //   }
-    // })
+    var nav = document.getElementById('navbar');
+    var inner_nav = document.getElementById('inner-navbar');
+    var empty_nav = document.getElementById('empty-navbar');
+    window.addEventListener("scroll", function () {
+      if (document.documentElement.scrollTop >= 150) {
+        nav.classList.add("fixed-top");
+        inner_nav.classList.add("container");
+        empty_nav.classList.add("do");
+        
+      } else {
+        nav.classList.remove("fixed-top");
+        inner_nav.classList.remove("container");
+        empty_nav.classList.remove("do");
+      }
+    })
   }
 }
