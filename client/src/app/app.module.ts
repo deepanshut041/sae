1d8ca500e149cb96aedbacf3aadf15b90e597b54
deepanshut041 <deepanshut041@gmail.com>
2018-01-05 14:17:01 +0530

    made basic skelton of the project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from "./core/core.module";
+import { HomeModule } from "./home/home.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, CoreModule
+    BrowserModule, CoreModule, HomeModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: []
+})
+export class HomeModule { }
