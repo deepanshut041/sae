c7294bb809b6e01f7890b9016a6b75c1c4ff3a8a
deepanshut041 <deepanshut041@gmail.com>
2018-01-09 12:40:44 +0530

    auth background changed...now  working on user dashboard

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
-  styleUrls: ["./auth.component.scss"]
+  styleUrls: ["./auth.component.css"]
 })
 
 export class AuthComponent implements OnInit {
-  
+  auth_background = 'assets/auth.gif'
   constructor() { 
 
   }
