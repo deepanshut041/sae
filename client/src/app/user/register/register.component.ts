a23d8fcb4d3711da40d0f8401eda9ebb24418792
deepanshut041 <deepanshut041@gmail.com>
2018-01-12 21:41:07 +0530

    made basic registration form now impleminting it

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"]
+  styleUrls: ["./register.component.css"]
 })
 
 export class RegisterComponent implements OnInit {
   
-  constructor() { 
+  registerForm:FormGroup
+  members:any[]
 
+  constructor(private fb:FormBuilder) { 
+    this.registerForm = this.fb.group({
+      'workshop':[null, Validators.required],
+      'plan':[null,Validators.required],
+      'members': this.fb.array([this.createItem()])
+    })
   }
 
   ngOnInit() {
 
+  }
+  createItem(): FormGroup {
+    return this.fb.group({
+      'username': [null,Validators.required],
+      'email': [null, Validators.required],
+      'contact': [null,Validators.required],
+      'college': [null,Validators.required],
+      'is_local': [null,Validators.required],
+    });
   }
 }
